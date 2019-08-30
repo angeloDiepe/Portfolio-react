f66f535bc35e5185877bb001eb54ae8e51011eed
angeloDiepe <angelodiepe10@yahoo.com>
2019-08-30 06:03:03 +0100

    adding react-particle-js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,10 +3,23 @@ import Nav from "../../components/Nav/Nav.js";
 import './App.css';
 import Particles from "react-particles-js";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area:2000
+      }
+    }
+  }
+}
+
 class App extends React.Component {
   render(){ 
     return (
       <div className="App">
+      <Particles params={{particlesOptions}} className="particles"/>
         <Nav/>
         {/*<Intro/>
                 <Desc/>

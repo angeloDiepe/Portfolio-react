aa2c4222ad714d19b33b0c5a3ee7bf6d1fcccf4b
angeloDiepe <angelodiepe10@yahoo.com>
2019-08-31 18:13:22 +0100

    design navbar and navbar responsiveness

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import "./Nav.css"
+import React,{useEffect} from 'react';
+import "./Nav.css";
+import imgLogo from "../../assets/logo.png";
 
 const Nav = (props) => {
-  return (
-    <div>
-    	
-    </div>
-  )
+	const showNav = () =>{
+		let navListWrapper = document.querySelector(".nav-list-wrapper");
+		let hamburger = document.querySelector(".hamburger");
+
+		navListWrapper.classList.toggle("open");
+		Array.from(navListWrapper.children).forEach(li => li.classList.toggle("fade"))
+		Array.from(hamburger.children).forEach(span => span.classList.toggle("open-hamburger"))
+	}
+/*	useEffect(() => {
+			let hamburger = document.querySelector("#hamburger");
+			console.log(hamburger)
+	}, [])*/
+	return (
+		<div className="nav-container">
+			<div id="nav-logo-wrapper">
+				<img src={imgLogo} alt="logo" id="nav-logo"/>
+			</div>
+			<div className="hamburger"	onClick={()=>showNav()} >
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<ul className="nav-list-wrapper">
+				<li className="nav-list-item"><a href="#"className="nav-link-item">Me</a></li>
+				<li className="nav-list-item"><a href="#" className="nav-link-item">Desc</a></li>
+				<li className="nav-list-item"><a href="#" className="nav-link-item">Skills</a></li>
+				<li className="nav-list-item"><a href="#" className="nav-link-item">Projects</a></li>
+				<li className="nav-list-item"><a href="#" className="nav-link-item">Contacts</a></li>
+			</ul>
+		</div>
+		)
 }
 
 export default Nav;
